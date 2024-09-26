c3bb36612f51221269ac39f31c37b972a168dc81
Jacek <0288007@schoolsnet.act.edu.au>
2024-09-26 17:24:18 +1000

    Why the fuck is it so hard to host my own damn website on my own

--- html/chatroom/chat.php
+++ html/chatroom/chat.php
@@ -1,18 +1,25 @@
 <?php
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nickname = htmlspecialchars($_POST['nickname']);
-    $message = htmlspecialchars($_POST['message']);
-    
-    $chatLog = fopen("chatlog.txt", "a");
-    fwrite($chatLog, "$nickname: $message\n");
-    fclose($chatLog);
-}
+if(isset($_POST['ajaxsend']) && $_POST['ajaxsend']==true){
+	// Code to save and send chat
+	$chat = fopen("chatdata.txt", "a");
+	$data="<b>".$_SESSION['username'].':</b> '.$_POST['chat']."<br>";
+	fwrite($chat,$data);
+	fclose($chat);
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if (file_exists("chatlog.txt")) {
-        echo nl2br(file_get_contents("chatlog.txt"));
-    }
+	$chat = fopen("chatdata.txt", "r");
+	echo fread($chat,filesize("chatdata.txt"));
+	fclose($chat);
+} else if(isset($_POST['ajaxget']) && $_POST['ajaxget']==true){
+	// Code to send chat history to the user
+	$chat = fopen("chatdata.txt", "r");
+	echo fread($chat,filesize("chatdata.txt"));
+	fclose($chat);
+} else if(isset($_POST['ajaxclear']) && $_POST['ajaxclear']==true){
+	// Code to clear chat history
+	$chat = fopen("chatdata.txt", "w");
+	$data="<b>".$_SESSION['username'].'</b> cleared chat<br>';
+	fwrite($chat,$data);
+	fclose($chat);
 }
-?>
